c5f3d24b77d1fb8fd5d18ad48cf733ddaddf4f0a
saied2035 <saied1998@gmail.com>
2023-03-19 00:32:41 +0200

    attach custom session controller to devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "devise_overrides/sessions"
+  }  
   resources :alerts, only: [:index]
   namespace :api do
     resources :alerts, only: [:index, :create]
