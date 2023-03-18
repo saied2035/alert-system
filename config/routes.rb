fc927e7662e1408043b0a28efdbc7e189329a25b
saied2035 <saied1998@gmail.com>
2023-03-18 19:21:01 +0200

    fix defination of api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :alerts, only: [:index]
-  scope :api do
+  namespace :api do
     resources :alerts, only: [:index, :create]
   end
   root "alerts#index"
