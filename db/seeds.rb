7686601c4d4213ea46f2cd627ec320e7face3855
saied2035 <saied1998@gmail.com>
2023-03-19 14:58:36 +0200

    remove fake data from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-Alert.create(type: "portal_opened", tag: ["verions2.3.9", "production"], description: "A portal was opened.", origin: "123.89.00.2")
-Alert.create(type: "portal_opened", tag: ["verions2.3.9", "production"], description: "A portal was opened.", origin: "123.89.00.2")
-Alert.create(type: "portal_opened", tag: ["verions2.3.9", "production"], description: "A portal was opened.", origin: "123.89.00.2")
-Alert.create(type: "portal_opened", tag: ["verions2.3.9", "production"], description: "A portal was opened.", origin: "123.89.00.2")
-Alert.create(type: "portal_opened", tag: ["verions2.3.9", "production"], description: "A portal was opened.", origin: "123.89.00.2")
-Alert.create(type: "portal_opened", tag: ["verions2.3.9", "production"], description: "A portal was opened.", origin: "123.89.00.2")
-Alert.create(type: "portal_opened", tag: ["verions2.3.9", "production"], description: "A portal was opened.", origin: "123.89.00.2")
-
-Alert.create(type: "portal_closed", tag: ["verions2.3.9", "production"], description: "A portal was closed.", origin: "123.89.00.2")
-Alert.create(type: "portal_closed", tag: ["verions2.3.9", "production"], description: "A portal was closed.", origin: "123.89.00.2")
-Alert.create(type: "portal_closed", tag: ["verions2.3.9", "production"], description: "A portal was closed.", origin: "123.89.00.2")
-Alert.create(type: "portal_closed", tag: ["verions2.3.9", "production"], description: "A portal was closed.", origin: "123.89.00.2")
-Alert.create(type: "portal_closed", tag: ["verions2.3.9", "production"], description: "A portal was closed.", origin: "123.89.00.2")
-Alert.create(type: "portal_closed", tag: ["verions2.3.9", "production"], description: "A portal was closed.", origin: "123.89.00.2")
-Alert.create(type: "portal_closed", tag: ["verions2.3.9", "production"], description: "A portal was closed.", origin: "123.89.00.2")
